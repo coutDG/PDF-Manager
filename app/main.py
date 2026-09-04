75503ec0b10c8a2789390c2aa8e1d7fcd4c2e2c5
coutDG <gdeep000bond@gmail.com>
2026-09-04 20:25:03 +0530

    'searchandviewadded'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 import sys
 import streamlit as st
 import os
+
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(BASE_DIR)
 from db.database import init_db
 from core.services import DocumentService
+
+if "search_results" not in st.session_state:
+    st.session_state['search_results'] = []
+if 'reader_mode' not in st.session_state:
+    st.session_state['reader_mode'] = False
+if 'selected_doc' not in st.session_state:
+    st.session_state['selected_doc'] = None
+if 'current_page' not in st.session_state:
+    st.session_state['current_page'] = 0
 
 init_db()
 
@@ -19,19 +29,92 @@ st.divider()
 tabs = st.tabs(["Upload", "Search & View", "Analytics"])
 
 with tabs[0]:
-    st.header('Upload PDF')
-    uploaded_file = st.file_uploader('Upload PDF' , type=['pdf']) #streamlit object
-    tags = st.text_input('Tags (comma seperated)')
-    description = st.text_area('Description')
-    lecture_date = st.date_input("Lecture Date (optional)", value = None)
-    if st.button('Upload'):
+    st.header("Upload PDF")
+    uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])  # streamlit object
+    tags = st.text_input("Tags (comma seperated)")
+    description = st.text_area("Description")
+    lecture_date = st.date_input("Lecture Date (optional)", value=None)
+    if st.button("Upload"):
         if uploaded_file and tags:
-            service.upload_document(uploaded_file,tags,description,lecture_date)
+            service.upload_document(uploaded_file, tags, description, lecture_date)
         else:
             st.error("Please upload the file")
 
 with tabs[1]:
-    pass
+    st.header("Search & View")
+    col1, col2 = st.columns(2)
+    with col1:
+        search_tag = st.text_input("Search by Tag")
+
+    with col2:
+        search_date = st.text_input("Search by Date", value=None)
+    if st.button("Search"):
+        st.session_state['search_results'] = service.search_documents(tag=search_tag if search_tag else None, date=str(search_date) if search_date else None)
+
+    results = st.session_state.search_results
+
+    if results and not st.session_state['reader_mode']:
+        st.subheader(f'Results : {len(results)} documents')
+        container = st.container(height = 500)
+
+        with container:
+            for doc in results:
+                col1,col2 = st.columns([1,3])
+
+
+                with col1:
+                    if doc.thumbnail_path:
+                        st.image(doc.thumbnail_path , width = 120)
+                with col2:
+                    st.write(f'***{doc.name}**')
+                    st.write(f'Tags:{doc.tags}')
+                    st.write(f'Description: {doc.description}')
+                    st.write(f'Lecture Date: {doc.lecture_date}')
+
+                    if st.button("Open",key = f'open_{doc.id}'):
+                        st.session_state.selected_doc = doc
+                        st.session_state.current_page = 0
+                        st.session_state['reader_mode'] = True
+                        st.rerun()
+
+    if st.session_state.reader_mode and st.session_state.selected_doc:
+            st.write('Reader Mode Active')
+            doc = st.session_state.selected_doc
+            st.subheader(f"Reading: {doc.name}")
+            folder_name = os.path.basename(doc.path).replace(".pdf",'')
+            image_dir = f"storage/pdf/{folder_name}"
+            st.write("Image dir:" , image_dir)
+
+            st.write('Files:', os.listdir(image_dir) if os.path.exists(image_dir) else "Not found" )
+
+            if not os.path.exists(image_dir):
+                st.error("Images not found , pdf conversion failed")
+            else:
+                images = sorted(os.listdir(image_dir))
+                #total_pages = len(pages)
+                total_pages = doc.total_pages
+                current_page = st.session_state.current_page
+
+                col1,col2,col3 = st.columns([1,2,1])
+
+                with col1:
+                    if st.button('Prev') and current_page>0:
+                        st.session_state.current_page-=1
+                        st.rerun()
+                with col2:
+                    st.write(f"Page {current_page + 1} / {total_pages}")
+                with col3:
+                    if st.button('Next') and current_page<total_pages-1:
+                        st.session_state.current_page+=1
+                        st.rerun()
+
+                img_path = os.path.join(image_dir,images[st.session_state.current_page])
+                st.image(img_path,width = "stretch")
+                st.write(f"FILE : {doc.name}")
+
+
+
+
 
 with tabs[2]:
     pass
--- core/services.py
+++ core/services.py
@@ -2,33 +2,51 @@
 # upload -> to pdf -> thumbnail -> folder with same name as pdf file -> images
 # total pages
 # upload_date -> datetime
+from datetime import datetime
 from db.repository import DocumentRepository
 from core.file_manager import FileManager
 from core.thumbnail import ThumbnailGenerator
+from core.reader import PDFReader
+from core.models import Document
 import os
 
 PDF_STORAGE = os.path.join('storage','pdf')
 
 class DocumentService:
     def __init__(self):
-        self.repo = DocumentRepository
+        self.repo = DocumentRepository()
         self.file_manager = FileManager()
         self.thumbnail_generator = ThumbnailGenerator()
-
+        self.reader= PDFReader()
     def upload_document(self,uploaded_file,tags,description,lecture_date = None):
-        doc = []
         
         file_path = self.file_manager.save_file(uploaded_file,tags,description,lecture_date)
 
         #generate thumbnail
-        thumbail_path = self.thumbnail_generator.generate_thumbail(file_path)
+        thumbnail_path = self.thumbnail_generator.generate_thumbail(file_path)
         #get total pages
         total_pages = self.thumbnail_generator.get_total_pages(file_path)
         #convert to images
-
+        image_paths = self.reader.convert_pdf_to_images(file_path)
         #required variables : upload_date
+
+        doc = Document(
+            id = None,
+            name = uploaded_file.name,
+            path=file_path,
+            thumbnail_path = thumbnail_path,
+            tags = tags,
+            description = description,
+            upload_date = datetime.now().strftime("%Y-%m-%d"),
+            lecture_date = lecture_date,
+            total_pages = total_pages
+        )
         #save to database
+        self.repo.add_document(doc)
         #self.repo.add_document(doc)
+    
+    def search_documents(self,tag = None, date = None):
+            return self.repo.search_document(tag,date)
 
 
 
